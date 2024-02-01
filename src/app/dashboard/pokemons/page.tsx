bbfb670bc2e219e7b5757872f266fa755ba242a0
cpandares <pandaresocesar@gmail.com>
2024-02-01 18:27:15 -0400

    page 500 error

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -8,12 +8,13 @@ const getPokemons = async(limit = 20, offset=0): Promise<SimplePokemon[]> =>{
     const data : PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
                 .then(res => res.json());
         
-  const pokemons =  data.results.map(pokemon => ({
-        id: pokemon.url.split('/').at(-2)!,
-        name: pokemon.name,
-       
-   }))
-      
+    const pokemons =  data.results.map(pokemon => ({
+          id: pokemon.url.split('/').at(-2)!,
+          name: pokemon.name,
+        
+    }));
+
+  // throw new Error('Error al obtener los pokemons, de ejemplo');         
    
    return pokemons;
 }
